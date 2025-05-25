7a7217627e758e61585603940917d79cf0607044
Gwendolyn <wilkistagwendolyn@gmail.com>
2025-05-25 15:58:36 +0300

    Created a print function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -29,3 +29,11 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+console.log(printTeacher("John", "Doe"));
